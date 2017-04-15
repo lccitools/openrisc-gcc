6d73e40092d4a9589861d2823b6f57b87b15a526
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2017-04-15 12:11:47 +0200

    Fix modulefile paths

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@
 FILE=or1k-elf-multicore_gcc${GCC_VERSION}_binutils${BINUTILS_VERSION}_newlib${NEWLIB_VERSION}_gdb${GDB_VERSION}.tgz
 wget https://github.com/openrisc/newlib/releases/download/v${NEWLIB_VERSION}/${FILE}
 
-mkdir -p ${OPENRISC_BASE}/${GCC_VERSION}-newlib${NEWLIB_VERSION}
-tar -xzf ${FILE} -C ${OPENRISC_BASE}/${GCC_VERSION}-newlib${NEWLIB_VERSION}
+OPENRISC_INSTALL=${OPENRISC_BASE}/${GCC_VERSION}-newlib${NEWLIB_VERSION}
+mkdir -p ${OPENRISC_INSTALL}
+tar -xzf ${FILE} -C ${OPENRISC_INSTALL}
 
 mkdir -p ${OPENRISC_MODULEFILES}
 cat <<EOF > ${OPENRISC_MODULEFILES}/${GCC_VERSION}-newlib${NEWLIB_VERSION}
 #%Module
-prepend-path PATH ${PREFIX}/or1k-elf-multicore/bin
+prepend-path PATH ${OPENRISC_INSTALL}/or1k-elf-multicore/bin
 EOF
